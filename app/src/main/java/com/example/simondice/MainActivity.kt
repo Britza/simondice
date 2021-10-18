a8e3e136e26f5479d58fa270ff811eae7c6a2e4c
Britza <bzegarraflores@danielcastelao.org>
2021-10-18 10:49:55 +0200

    Metodos creados

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,28 @@ class MainActivity : AppCompatActivity() {
         val empezaraJugar:Button = findViewById(R.id.jugar)
 
         empezaraJugar.setOnClickListener{
+            Log.INFO
 
+            mostrarRonda()
+            ejecutarSecuencia()
         }
+
+
+    }
+    private fun mostrarRonda(){
+
+    }
+
+    private fun ejecutarSecuencia(){
+
+    }
+
+    private fun mensajeUsuario(){
+
+    }
+
+    private fun comprobarSecuencia(){
+
     }
 
 
